30c8aef64731c31823455db44946b01e62191141
Ilho Kim <isagum@gmail.com>
2019-02-08 11:57:21 -0800

    feat: print contents fo the Piggy Bank complete

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -7,15 +7,17 @@ public class Main {
 
     private static void account() {
         // Pseudo code
-//        piggyBank.add(new Quarter())
-//        piggyBank.add(new Dime())
-//        piggyBank.add(new Dollar(5))
-//        piggyBank.add(new Nickle(3))
-//        piggyBank.add(new Dime(7))
-//        piggyBank.add(new Dollar())
-//        piggyBank.add(new Penny(10))
-//        Print the contents of the Piggy Bank
-        Map<String, Integer> piggyBank = new HashMap<>();
+        PiggyBank piggyBank = new PiggyBank();
+
+        piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
+
+        piggyBank.inquiryAccount();
 
     }
 
--- src/com/lambdaschool/PiggyBank.java
+++ src/com/lambdaschool/PiggyBank.java
@@ -1,10 +1,24 @@
 package com.lambdaschool;
 
+import java.util.ArrayList;
+
 public class PiggyBank {
 
+    private ArrayList<UsCurrency> account = new ArrayList<>();
     private double balance;
 
     public void add(UsCurrency deposit) {
         balance = balance + (deposit.getCount() * deposit.getParValue());
+        account.add(deposit);
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void inquiryAccount() {
+        for (UsCurrency c: account) {
+            System.out.println(c.getCount() + " " + c.getCommonName());
+        }
     }
 }
