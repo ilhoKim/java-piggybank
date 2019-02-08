b9696b1c58ee27597f2207f9e6de791f60391704
Ilho Kim <isagum@gmail.com>
2019-02-08 11:37:14 -0800

    fix: fixed typo

--- src/com/lambdaschool/PiggyBank.java
+++ src/com/lambdaschool/PiggyBank.java
@@ -5,6 +5,6 @@ public class PiggyBank {
     private double balance;
 
     public void add(UsCurrency deposit) {
-        balance = balance + (deposit.count * deposit.parValue);
+        balance = balance + (deposit.getCount() * deposit.getParValue());
     }
 }
