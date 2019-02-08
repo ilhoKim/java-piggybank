68c5d887cfa1935d38e0f13893554fdf289d42d5
Ilho Kim <isagum@gmail.com>
2019-02-08 11:35:56 -0800

    feat: implement deposition adding up balance

--- src/com/lambdaschool/PiggyBank.java
+++ src/com/lambdaschool/PiggyBank.java
@@ -1,6 +1,10 @@
 package com.lambdaschool;
 
 public class PiggyBank {
+
     private double balance;
 
+    public void add(UsCurrency deposit) {
+        balance = balance + (deposit.count * deposit.parValue);
+    }
 }
